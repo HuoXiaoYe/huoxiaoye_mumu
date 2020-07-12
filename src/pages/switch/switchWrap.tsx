4b21587c2d82c52e6a6dcdbcb46a7195765db71c
unityzhang <unity@tencent.com>
2020-07-12 11:46:21 +0800

    switch finish

--- src/pages/switch/switchWrap.tsx
+++ src/pages/switch/switchWrap.tsx
@@ -9,10 +9,22 @@ const SwitchWrap: React.FC<any> = () => {
     return (
         <div className="switch-wrap">
             <div className="switch-item">
-                <span>不同尺寸Switch</span>
+                <span>三种尺寸</span>
                 <Switch checked={false} size="big" onChange={handleChang1}/>
                 <Switch checked={false} size="normal" onChange={()=>{}}/>
                 <Switch checked={false} size="small" onChange={()=>{}}/>
+            </div>
+            <div className="switch-item">
+                <span>不可选中</span>
+                <Switch checked={false} disabled="disabled" size="big" onChange={handleChang1}/>
+                <Switch checked={true} disabled="disabled" size="normal" onChange={()=>{}}/>
+                <Switch checked={false} disabled="disabled" size="small" onChange={()=>{}}/>
+            </div>
+            <div className="switch-item">
+                <span>loading状态</span>
+                <Switch checked={false} loading="loading" size="big" onChange={handleChang1}/>
+                <Switch checked={true} loading="loading" disabled="disabled" size="normal" onChange={()=>{}}/>
+                <Switch checked={false} loading="loading" size="small" onChange={()=>{}}/>
             </div>
         </div>
     )
--- src/ui/components/switch/switch.tsx
+++ src/ui/components/switch/switch.tsx
@@ -10,19 +10,23 @@ export interface SwitchProps {
     size?: switchType;
     bgColor?: string;
     activeColor?: string;
-    disabled?: boolean;
+    disabled?: 'disabled';
+    loading?:"loading";
     onChange?: React.ChangeEventHandler<HTMLInputElement>
 }
 
 
-const Switch: React.FC<SwitchProps> = ({ className = "", checked = false, size, disabled, onChange }) => {
+const Switch: React.FC<SwitchProps> = ({ className = "", checked = false, size, disabled, onChange,loading }) => {
     const [switchID] = useState<string>('switch-' + Math.floor(Math.random() * 999999))
     const classes = generateClassName("mu", {
-        [`${size}`]: !!size
+        [`${size}`]: !!size,
+        [`${disabled}`]:!!disabled,
+        [`${loading}`]:!!loading
     }, "mu-switch")
+    console.log(classes)
     const [isChecked, setChecked] = useState<boolean>(checked)
     function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-        if (disabled) {
+        if (disabled || loading) {
             return
         }
         setChecked(e.target.checked)
@@ -33,7 +37,9 @@ const Switch: React.FC<SwitchProps> = ({ className = "", checked = false, size, 
     return (
         <span className={className + " mu-switch-container"}>
             <input className="hello" id={switchID} checked={isChecked} type="checkbox" onChange={handleChange} />
-            <label htmlFor={switchID} className={classes}></label>
+            <label htmlFor={switchID} className={classes}>
+                {loading ? <div className="loading"></div>:""}
+            </label>
         </span>
     )
 }
